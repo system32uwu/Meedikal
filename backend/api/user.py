005fd3053b54fd632f8810fa440a05ca1b749de5
system32uwu <mateocarriqui7@gmail.com>
2021-08-22 18:27:21 -0300

    Simplified api routing

--- backend/api/user.py
+++ backend/api/user.py
@@ -1,16 +1,12 @@
 from flask import json, jsonify, Blueprint, request
 from models.User import User
+
 router = Blueprint('user', __name__, url_prefix='/user')
 
-@router.route('', methods=['GET']) # GET /api/user
+@router.route('/all', methods=['GET']) # GET /api/user/all
 def allUsers():
-    return jsonify(User.query.all())
+    return jsonify(User.query.all()), 200
 
-@router.route('', methods=['POST']) # POST /api/user
-def userByCi():
-    try:
-        userData = json.loads(request.data)
-        ci = userData['ci']
-        return jsonify(User.query.filter(User.ci == ci).first())   
-    except:
-        return 'missing ci', 400 # if there was no request.data or userData['ci], return this.
+@router.route('/<int:ci>', methods=['GET']) # GET /api/user/1
+def userByCi(ci):
+    return jsonify(User.query.filter(User.ci == ci).first())

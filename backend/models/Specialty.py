785e887af9e46eaf32a1e1e12aa23961370ee8f5
system32uwu <mateocarriqui7@gmail.com>
2021-09-07 01:38:49 -0300

    Specialty classes cleanup

--- backend/models/Specialty.py
+++ backend/models/Specialty.py
@@ -1,25 +1,17 @@
 from dataclasses import dataclass
-from sqlalchemy.orm import relationship
-from .db import db, BaseModel
-from .User import MedicalPersonnel
+from typing import Optional
 
 @dataclass
-class Specialty(BaseModel):
+class Specialty:
+    __tablename__ = 'specialty'
 
     id: int
     title: str
 
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.VARCHAR(64), unique=True, nullable=False) # It wouldn't make sense to store specialties with the same name multiple times, but neither would setting a 64 characters long PK do so.
-    
 @dataclass
-class MpHasSpec(BaseModel): 
+class MpHasSpec: 
     __tablename__ = 'mpHasSpec'
     
     ciMp: int
     idSpec: int
-    detail: str
-
-    ciMp = db.Column(db.Integer, db.ForeignKey(MedicalPersonnel.ci, ondelete='CASCADE'), primary_key=True)
-    idSpec = db.Column(db.Integer, db.ForeignKey(Specialty.id, ondelete='CASCADE'), primary_key=True)
-    detail = db.Column(db.VARCHAR(128))
+    detail: Optional[str]

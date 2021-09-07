c91a5394c14b397b500010443f0e8579588dd74d
system32uwu <mateocarriqui7@gmail.com>
2021-09-07 01:35:00 -0300

    Branch and ClinicalSign classes cleanup

--- backend/models/Branch.py
+++ backend/models/Branch.py
@@ -1,47 +1,17 @@
-from .Treatment import TakesCare
-from .Appointment import Appointment
 from dataclasses import dataclass
-from .db import db, BaseModel
 
 @dataclass
-class Branch(BaseModel):
+class Branch:
+    __tablename__ = 'branch'
 
     id: int
     name: str
     phoneNumber: str
     location: str
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.VARCHAR(64), unique=True, nullable=False) 
-    phoneNumber = db.Column(db.VARCHAR(64), nullable=False) 
-    location = db.Column(db.VARCHAR(64), nullable=False) 
-
 @dataclass
-class ApTakesPlace(BaseModel): # Appointment < apTakesPlace > Branch
+class ApTakesPlace: # Appointment < apTakesPlace > Branch
     __tablename__ = 'apTakesPlace'
 
     idAp: int
     idBranch: int
-
-    idAp = db.Column(db.Integer, db.ForeignKey(Appointment.id, ondelete='CASCADE'), primary_key=True)
-    idBranch = db.Column(db.Integer, db.ForeignKey(Branch.id, ondelete='CASCADE'))
-
-@dataclass
-class TrTakesPlace(BaseModel): # takesCare < trTakesPlace > Branch
-    __tablename__ = 'trTakesPlace'
-
-    idFollows: int
-    idTreatment: int
-    ciPa: int
-    ciMp: int
-    date: int
-    idBranch: int
-
-    idFollows = db.Column(db.Integer, primary_key=True)
-    idTreatment = db.Column(db.Integer, primary_key=True)
-    ciPa = db.Column(db.Integer, primary_key=True)
-    ciMp = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime, primary_key=True)
-    idBranch = db.Column(db.Integer, db.ForeignKey(Branch.id, ondelete='CASCADE'))
-
-    __table_args__ = (db.ForeignKeyConstraint([idFollows,idTreatment,ciPa,ciMp,date],[TakesCare.idFollows,TakesCare.idTreatment,TakesCare.ciPa,TakesCare.ciMp,TakesCare.date]),)
--- backend/models/ClinicalSign.py
+++ backend/models/ClinicalSign.py
@@ -1,31 +1,18 @@
-from .Appointment import AttendsTo
 from dataclasses import dataclass
-from .db import db, BaseModel
 
 @dataclass
-class ClinicalSign(BaseModel):
+class ClinicalSign:
     __tablename__ = 'clinicalSign'
 
     id: int
     name: str
     description: str
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.VARCHAR(128), unique=True, nullable=False) 
-    description = db.Column(db.VARCHAR(512)) 
-
 @dataclass
-class RegistersCs(BaseModel): # { Patient < attendsTo > [ Doctor < assignedTo > Appointment] } < registersCs > ClinicalSign
+class RegistersCs: # { Patient < attendsTo > [ Doctor < assignedTo > Appointment] } < registersCs > ClinicalSign
     __tablename__ = 'registersCs'
 
     idAp: int
     ciPa: int
     idCs: int
     detail: str
-
-    idAp = db.Column(db.Integer, primary_key=True)
-    ciPa = db.Column(db.Integer, primary_key=True)
-    idCs = db.Column(db.Integer, db.ForeignKey(ClinicalSign.id, ondelete='CASCADE'), primary_key=True)
-    detail = db.Column(db.VARCHAR(256))
-
-    __table_args__ = (db.ForeignKeyConstraint([idAp,ciPa], [AttendsTo.idAp,AttendsTo.ciPa], ondelete='CASCADE'),)

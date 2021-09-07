d85e5df6b51285e45bfb70d793d1a5ea740056c0
system32uwu <mateocarriqui7@gmail.com>
2021-09-07 01:39:44 -0300

    Symptom classes cleanup

--- backend/models/Symptom.py
+++ backend/models/Symptom.py
@@ -1,30 +1,18 @@
-from .Appointment import AttendsTo
 from dataclasses import dataclass
-from .db import db, BaseModel
+from typing import Optional
 
 @dataclass
-class Symptom(BaseModel):
+class Symptom:
     
     id: int
     name: str
-    description: str
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.VARCHAR(128), nullable=False) 
-    description = db.Column(db.VARCHAR(512)) 
+    description: Optional[str]
 
 @dataclass
-class RegistersSy(BaseModel): # { Patient < attendsTo > [ Doctor < assignedTo > Appointment] } < registersSy > Symptom
+class RegistersSy: # { Patient < attendsTo > [ Doctor < assignedTo > Appointment] } < registersSy > Symptom
     __tablename__ = 'registersSy'
 
     idAp: int
     ciPa: int
     idSy: int
-    detail: str
-
-    idAp = db.Column(db.Integer, primary_key=True)
-    ciPa = db.Column(db.Integer, primary_key=True)
-    idSy = db.Column(db.Integer, db.ForeignKey(Symptom.id, ondelete='CASCADE'), primary_key=True)
-    detail = db.Column(db.VARCHAR(256))
-
-    __table_args__ = (db.ForeignKeyConstraint([idAp,ciPa], [AttendsTo.idAp,AttendsTo.ciPa], ondelete='CASCADE'),)
+    detail: Optional[str]

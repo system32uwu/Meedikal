bf1537eef2d725f2275b607312cc90560f7b485e
system32uwu <mateocarriqui7@gmail.com>
2021-09-07 01:21:39 -0300

    Added Optional typing to fields in user classes

--- backend/models/User.py
+++ backend/models/User.py
@@ -1,25 +1,25 @@
 from dataclasses import dataclass
-from sqlalchemy.orm import relationship
 from werkzeug.security import check_password_hash
 from .db import db
 from datetime import datetime
+from typing import Optional
 
 @dataclass
-class User():
+class User:
     __tablename__ = 'user'
     
     ci: int
     name1: str
-    name2: str
+    name2: Optional[str]
     surname1: str
-    surname2: str
+    surname2: Optional[str]
     sex: str
-    genre: str
+    genre: Optional[str]
     birthdate: datetime
     location: str
     email: str
-    active: bool
     password: str
+    active: Optional[bool] = True
 
     @classmethod
     def getByCi(cls, ci:int):
@@ -29,72 +29,46 @@ class User():
         return check_password_hash(self.password, password)
 
 @dataclass # since phone is a multivalued attribute, it has its own table.
-class UserPhone(BaseModel):
+class UserPhone:
     __tablename__ = 'userPhone'
 
     ci: int
     phone: str # since phone numbers aren't real numbers it's better to store them as strings. Some countries (like Uruguay), start their cellphone numbers with a 0, which on input would be ignored by the DBMS if the datatype was Integer. 
 
-    ci = db.Column(db.Integer, db.ForeignKey(User.ci, ondelete='CASCADE'), primary_key=True)
-    phone = db.Column(db.VARCHAR(32), primary_key=True) # some countries' phone numbers are quite long 
-
 @dataclass
-class Patient(BaseModel):
+class Patient:
+    __tablename__ = 'patient'
 
     ci: int
-
-    ci = db.Column(db.Integer, db.ForeignKey(User.ci, ondelete='CASCADE'), primary_key=True)
-
-    base: User = relationship('User') # in order to get the base attributes
     
 @dataclass # users from the medical personnel, those without further categorization (either doctor or medical assitant), will be stored only in this table and have limited permissions and access
-class MedicalPersonnel(BaseModel):
+class MedicalPersonnel:
     __tablename__ = 'medicalPersonnel'
 
     ci: int
 
-    ci = db.Column(db.Integer, db.ForeignKey(User.ci, ondelete='CASCADE'), primary_key=True)
-
-    base: User = relationship('User') # in order to get the base attributes
-
 @dataclass # users from the medical personnel, who are doctors. 
-class Doctor(BaseModel):
+class Doctor:
+    __tablename__ = 'doctor'
 
     ci: int
 
-    ci = db.Column(db.Integer, db.ForeignKey(MedicalPersonnel.ci, ondelete='CASCADE'), primary_key=True)
-
-    base = relationship('MedicalPersonnel') # in order to get the base attributes
-
 @dataclass # users from the medical personnel, who are medical assistants (i.e: nurses)
-class MedicalAssitant(BaseModel):
+class MedicalAssitant:
     __tablename__ = 'medicalAssistant'
 
     ci: int
 
-    ci = db.Column(db.Integer, db.ForeignKey(MedicalPersonnel.ci, ondelete='CASCADE'), primary_key=True)
-
-    base = relationship('MedicalPersonnel') # in order to get the base attributes
-
 @dataclass
-class Administrative(BaseModel):
+class Administrative:
 
     ci: int
 
-    ci = db.Column(db.Integer, db.ForeignKey(User.ci, ondelete='CASCADE'), primary_key=True)
-
-    base: User = relationship('User') # in order to get the base attributes
-
 @dataclass
-class UIsRelatedTo(BaseModel): # User1 < uIsRelatedTo > User2
+class UIsRelatedTo: # User1 < uIsRelatedTo > User2
     __tablename__ = 'uIsRelatedTo'
     
     _id:int
     user1: int
     user2: int
-    relationType: str # user1 is father of user2
-
-    _id = db.Column(db.Integer,primary_key=True, autoincrement=True)
-    user1 = db.Column(db.Integer, db.ForeignKey(User.ci, ondelete='CASCADE'), primary_key=True)
-    user2 = db.Column(db.Integer, db.ForeignKey(User.ci, ondelete='CASCADE'), primary_key=True)
-    relationType = db.Column(db.VARCHAR(32))
+    relationType: Optional[str] # user1 is father of user2

0503cbc69ed6170a7f7926ba3e89a5c159cb4584
system32uwu <mateocarriqui7@gmail.com>
2021-10-10 03:25:00 -0300

    run script working correctly

--- run.ps1
+++ run.ps1
@@ -1,13 +1,21 @@
+$ErrorActionPreference= 'silentlycontinue'
+
 Set-Location backend
 
-if (-not $Env:MEEDIKAL_HAS_RUN_YET){
+$run = $Env:MEEDIKAL_HAS_RUN_YET
+
+if ((-not $run) -or ($run -eq 0)) {
     python -m venv venv 
 
     ./venv/Scripts/activate
     pip install -r requirements.txt
 
     Move-Item .example.env .env
+    Move-Item config.example.py config.py
     $Env:MEEDIKAL_HAS_RUN_YET = 1
+}
+else {
+    ./venv/Scripts/activate
 }
 
 flask run | Start-Process "http://localhost"

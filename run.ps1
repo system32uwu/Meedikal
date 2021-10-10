5a31e87e7e33ce44f937f7dbe85a369c2f00fcb4
system32uwu <mateocarriqui7@gmail.com>
2021-10-10 03:04:42 -0300

    better script

--- run.ps1
+++ run.ps1
@@ -1,9 +1,12 @@
 Set-Location backend
 
-python -m venv venv 
+if (-not $Env:MEEDIKAL_HAS_RUN_YET){
+    python -m venv venv 
 
-pip install -r requirements.txt
+    pip install -r requirements.txt
 
-Move-Item .example.env .env
+    Move-Item .example.env .env
+    $Env:MEEDIKAL_HAS_RUN_YET = 1
+}
 
 flask run | Start-Process "http://localhost"

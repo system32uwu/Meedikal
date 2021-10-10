4cd43f3831ec5dbe3694e48864631d17331a0728
system32uwu <mateocarriqui7@gmail.com>
2021-10-10 03:07:10 -0300

    activated venv

--- run.ps1
+++ run.ps1
@@ -3,6 +3,7 @@ Set-Location backend
 if (-not $Env:MEEDIKAL_HAS_RUN_YET){
     python -m venv venv 
 
+    ./venv/Scripts/activate
     pip install -r requirements.txt
 
     Move-Item .example.env .env

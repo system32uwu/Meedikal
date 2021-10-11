11a3a0c70c4c56e1a9ed9d629969849d1abb639f
system32uwu <mateocarriqui7@gmail.com>
2021-10-11 16:47:53 -0300

    added code to build css in run.ps1

--- run.ps1
+++ run.ps1
@@ -5,14 +5,18 @@ Set-Location backend
 $run = $Env:MEEDIKAL_HAS_RUN_YET
 
 if ((-not $run) -or ($run -eq 0)) {
-    python -m venv venv 
+    python -m venv venv # create virtual environment #
 
-    ./venv/Scripts/activate
-    pip install -r requirements.txt
+    ./venv/Scripts/activate # activate virtual environment #
+    pip install -r requirements.txt # install requirements #
 
-    Move-Item .example.env .env
-    Move-Item config.example.py config.py
-    ./util/createDb.py && ./util/createAdmin.py
+    Move-Item .example.env .env # replace dotenv file #
+    Move-Item config.example.py config.py # replace config file #
+    ./util/createDb.py && ./util/createAdmin.py # create db and admin #
+    
+    Set-Location statc/styles 
+    npm install # install dependencies #
+    npm run build:css # build and minify tailwindcss file #
     $Env:MEEDIKAL_HAS_RUN_YET = 1
 }
 else {

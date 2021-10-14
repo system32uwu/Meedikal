08c0194f1801960272e1867cb3b083c198791311
system32uwu <mateocarriqui7@gmail.com>
2021-10-14 19:30:45 -0300

    async api calls from /users

--- backend/frontend/static/js/util.js
+++ backend/frontend/static/js/util.js
@@ -1,37 +1,37 @@
 const generateColumn = (colName) => {
-    return `
+  return `
     <th
     scope="col"
     id="${colName}"
     class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
     ${colName}
-    </th>`
+    </th>`;
 };
 
 const generateCell = (value) => {
-    return `
+  return `
     <td class="px-6 py-4 whitespace-nowrap">
     <div class="text-sm text-gray-900 text-center">
         ${value}
     </div>
     </td>`;
-} //generic cell generator
+}; //generic cell generator
 
 const generateRow = (cells) => {
-    return `
+  return `
     <tr>
         ${cells.join("\n")}
     </tr>
     `;
-}
+};
 
 const generateTable = (tableId, colNames, rows) => {
-    return `
+  return `
     <div class="border-b border-gray-200 sm:rounded-lg overflow-y-auto h-full">
         <table class="divide-y divide-gray-200 w-full h-full" id="${tableId}">
             <thead class="bg-gray-50">
                 <tr>
-                    ${colNames.map((name) => generateColumn(name)).join('\n')}
+                    ${colNames.map((name) => generateColumn(name)).join("\n")}
                 </tr>
             </thead>
             <tbody>
@@ -40,37 +40,44 @@ const generateTable = (tableId, colNames, rows) => {
         </table>
     </div>
     `;
-}
+};
 
-const setPagination = (tablename, offset, limit, paginationContainer, fn) => {
-    fetch(`/api/pagination/total/${tablename}`).then((res) => {
-      if (res.status === 200){
-        res.json().then(data => {
-          total = data.result;
-  
-          if (total < limit){
-            limit = total;
-          }
+const setPagination = async (
+  tablename,
+  offset,
+  limit,
+  paginationContainer,
+  fn
+) => {
+  res = await fetch(`/api/pagination/total/${tablename}`);
+  data = await res.json();
 
-          let itemsPerPage = limit - offset;
-  
-          let pageCount = total/itemsPerPage;
-  
-          let btns = Array(pageCount);
-          
-          for (let i = 0; i < pageCount; i++){
-            btns.push(`
+  if (res.status === 200) {
+    total = data.result;
+
+    if (total < limit) {
+      limit = total;
+    }
+
+    let itemsPerPage = limit - offset;
+
+    let pageCount = total / itemsPerPage;
+
+    let btns = Array(pageCount);
+
+    for (let i = 0; i < pageCount; i++) {
+      btns.push(`
               <button 
               class="rounded-xl bg-turqoise text-white font-bold px-2 text-center"
-              onclick="${fn}(${itemsPerPage*i})">
+              onclick="${fn}(${itemsPerPage * i})">
               ${i + 1}
               </button>
-            `)
-          }
-          paginationContainer.innerHTML = btns.join('\n')
-          //show pagination
-          Promise.resolve('OK')
-        })
-      }
-    });
+            `);
+    }
+    paginationContainer.innerHTML = btns.join("\n");
+    //show pagination
+    return total;
+  } else {
+    Promise.reject(res.data);
   }
+};
